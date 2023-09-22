8f64ed6fe0d6c15b3acc521075298ca1da12b70e
Ka Sen Low <lowkasen@gmail.com>
2023-09-22 19:27:56 +0800

    added app-of-apps

--- 04-argocd-application-deployment.tf
+++ 04-argocd-application-deployment.tf
@@ -1,21 +1,49 @@
-resource "kubectl_manifest" "app-helm-guestbook-ns" {
+resource "kubectl_manifest" "apps-ns" {
   yaml_body = <<YAML
 apiVersion: v1
 kind: Namespace
 metadata:
-  name: app-helm-guestbook
+  name: apps
   annotations:
     argocd.argoproj.io/sync-wave: "-1"
 YAML
 }
 
-resource "kubectl_manifest" "helm-guestbook" {
-  depends_on = [kubectl_manifest.app-helm-guestbook-ns]
+# resource "kubectl_manifest" "helm-guestbook" {
+#   depends_on = [kubectl_manifest.app-helm-guestbook-ns]
+#   yaml_body  = <<YAML
+# apiVersion: argoproj.io/v1alpha1
+# kind: Application
+# metadata:
+#   name: helm-guestbook
+#   namespace: argocd
+#   finalizers:
+#     # The default behaviour is foreground cascading deletion
+#     - resources-finalizer.argocd.argoproj.io
+#     # Alternatively, you can use background cascading deletion
+#     # - resources-finalizer.argocd.argoproj.io/background
+# spec:
+#   project: default
+#   source:
+#     repoURL: https://github.com/lowkasen/sandbox01-eks
+#     path: apps/helm-guestbook
+#   destination:
+#     server: "https://kubernetes.default.svc"
+#     namespace: app-helm-guestbook
+#   syncPolicy:
+#     automated:
+#       prune: false
+#       selfHeal: false
+# YAML
+# }
+
+esource "kubectl_manifest" "app-of-apps" {
+  depends_on = [kubectl_manifest.apps-ns]
   yaml_body  = <<YAML
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
-  name: helm-guestbook
+  name: app-of-apps
   namespace: argocd
   finalizers:
     # The default behaviour is foreground cascading deletion
@@ -26,13 +54,10 @@ spec:
   project: default
   source:
     repoURL: https://github.com/lowkasen/sandbox01-eks
-    # targetRevision: main
-    path: apps/helm-guestbook
-    # repoURL: https://github.com/argoproj/argocd-example-apps
-    # path: helm-guestbook
+    path: apps-of-apps
   destination:
     server: "https://kubernetes.default.svc"
-    namespace: app-helm-guestbook
+    namespace: apps
   syncPolicy:
     automated:
       prune: false
