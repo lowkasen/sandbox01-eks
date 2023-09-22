73f590f8860c703736c1dba455419057f8064d8e
Ka Sen Low <lowkasen@gmail.com>
2023-09-22 17:24:07 +0800

    added application deployment and namespace

--- 04-argocd-application-deployment.tf
+++ 04-argocd-application-deployment.tf
@@ -3,3 +3,37 @@
 #   repository = "https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd"
 #   chart = 
 # }
+
+resource "kubectl_manifest" "app-helm-guestbook-ns" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: app-helm-guestbook
+  annotations:
+    argocd.argoproj.io/sync-wave: "-1"
+YAML
+}
+
+resource "kubectl_manifest" "helm-guestbook" {
+  depends_on = [kubectl_manifest.app-helm-guestbook-ns]
+  yaml_body  = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: helm-guestbook
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/argoproj/argocd-example-apps
+    path: helm-guestbook
+  destination:
+    server: "https://kubernetes.default.svc"
+    namespace: app-helm-guestbook
+    syncPolicy:
+    automated:
+      prune: false
+      selfHeal: false
+YAML
+}
